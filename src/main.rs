d1e1f86749fd4b7f219735f95906dcc7babe9b57
somemorewater <oboyiloejeh@gmail.com>
2026-04-24 00:45:33 +0100

    refactor: restructure AppState and add get_tests function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
-use axum::{Json, Router, routing::{get, post}};
+use axum::{
+    Json, Router,
+    extract::State,
+    routing::{get, post},
+};
 use serde_json::json;
-
+use std::sync::{Arc, Mutex};
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Clone)]
+struct AppState {
+    tests: Arc<Mutex<Vec<Test>>>,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
 struct Test {
     title: String,
     subject: String,
@@ -15,10 +24,15 @@ async fn health() -> Json<serde_json::Value> {
 
 #[tokio::main]
 async fn main() {
+    let state = AppState {
+        tests: Arc::new(Mutex::new(Vec::new())),
+    };
+
     let app = Router::new()
         .route("/", get(home))
-        .route("/test", post(create_test))
-        .route("/health", get(health));
+        .route("/tests", get(get_tests).post(create_test))
+        .route("/health", get(health))
+        .with_state(state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
@@ -34,11 +48,22 @@ async fn home() -> Json<serde_json::Value> {
     }))
 }
 
+async fn get_tests(
+    State(state): State<AppState>,
+) -> Json<Vec<Test>> {
+    let tests = state.tests.lock().unwrap().clone();
+
+    Json(tests)
+}
+
 async fn create_test(
+    State(state): State<AppState>,
     Json(payload): Json<Test>,
 ) -> Json<serde_json::Value> {
+
+    state.tests.lock().unwrap().push(payload);
+
     Json(json!({
         "message": "Test created",
-        "data": payload
     }))
 }
