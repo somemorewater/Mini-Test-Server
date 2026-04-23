de053fea26b0efabe76494df0b3c53c7342dfb28
somemorewater <oboyiloejeh@gmail.com>
2026-04-23 23:43:17 +0100

    add home function

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ async fn health() -> Json<serde_json::Value> {
 #[tokio::main]
 async fn main() {
     let app = Router::new()
+        .route("/", get(home))
         .route("/health", get(health));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -15,4 +16,11 @@ async fn main() {
     println!("Server running on port 3000");
 
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn home() -> Json<serde_json::Value> {
+    Json(json!({
+        "message": "Mini Test Server",
+        "version": 1
+    }))
 }
