cc35e3d1632d312be2d408eff2a63b8053a7593c
somemorewater <oboyiloejeh@gmail.com>
2026-04-24 00:16:42 +0100

    add create_test function and update routes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
-use axum::{Json, Router, routing::get, serve::Listener};
+use axum::{Json, Router, routing::{get, post}};
 use serde_json::json;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Test {
+    title: String,
+    subject: String,
+}
 
 async fn health() -> Json<serde_json::Value> {
     Json(json!({"status": "ok"}))
@@ -9,6 +17,7 @@ async fn health() -> Json<serde_json::Value> {
 async fn main() {
     let app = Router::new()
         .route("/", get(home))
+        .route("/test", post(create_test))
         .route("/health", get(health));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -22,5 +31,14 @@ async fn home() -> Json<serde_json::Value> {
     Json(json!({
         "message": "Mini Test Server",
         "version": 1
+    }))
+}
+
+async fn create_test(
+    Json(payload): Json<Test>,
+) -> Json<serde_json::Value> {
+    Json(json!({
+        "message": "Test created",
+        "data": payload
     }))
 }
